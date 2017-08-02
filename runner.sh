ba78f827e238ce2f4bfdb6b6135a24648a0a4760
Erik Seifert <erik.seifert@b-connect.de>
2017-08-02 15:58:41 +0200

    Update

--- runner.sh
+++ runner.sh
@@ -6,4 +6,6 @@ fi
 
 echo "$SSH_KEY" > /root/.ssh/id_rsa && chmod 600 /root/.ssh/id_rsa
 
+echo "PROCESSING USING BUILDFILE $PLUGIN_BUILDFILE"
+
 phing -f $PLUGIN_BUILDFILE -Dsettings.tag=$DRONE_TAG
