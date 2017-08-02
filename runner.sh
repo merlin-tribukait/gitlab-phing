30c181a4778be7f6ea012158259b283e688d62c7
Erik Seifert <erik.seifert@b-connect.de>
2017-08-02 16:10:30 +0200

    Update

--- runner.sh
+++ runner.sh
@@ -6,7 +6,7 @@ fi
 
 cd $DRONE_WORKSPACE
 if [ -z "$PLUGIN_BUILDFILE" ]; then
-    export PLUGIN_BUILDFILE="./build.yml"
+    export PLUGIN_BUILDFILE="./build.xml"
 fi
 
 echo "$SSH_KEY" > /root/.ssh/id_rsa && chmod 600 /root/.ssh/id_rsa
