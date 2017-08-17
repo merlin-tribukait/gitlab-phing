646e6af0097d273964bf13631b59b00720e5728b
Erik Seifert <erik.seifert@b-connect.de>
2017-08-17 17:40:46 +0200

    Update

--- runner.sh
+++ runner.sh
@@ -15,8 +15,7 @@ echo "PROCESSING USING BUILDFILE $PLUGIN_BUILDFILE"
 
 if [[ ${VPN_PASS} ]]; then
     echo "CONNECT TO VPN $VPN_URL $VPN_USER"
-    echo $VPN_PASS | openconnect $VPN_URL -u $VPN_USER --no-xmlpost --juniper --passwd-on-stdin -b
-    echo "Connected"
+    echo $VPN_PASS | nohup openconnect $VPN_URL -u $VPN_USER --no-xmlpost --juniper --passwd-on-stdin &
 fi
 
 echo "Execute phing now."
