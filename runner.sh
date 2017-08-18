7eb4c8de2a7352bf816e0819e3d8fd5165201333
Erik Seifert <erik.seifert@b-connect.de>
2017-08-18 15:27:28 +0200

    Set new docker parent bconnect/phing

--- runner.sh
+++ runner.sh
@@ -1,17 +1,12 @@
 #!/bin/sh
-if [ -z "$DRONE_WORKSPACE" ]; then
-    export DRONE_WORKSPACE="/"
-fi
-
 
 cd $DRONE_WORKSPACE
 if [ -z "$PLUGIN_BUILDFILE" ]; then
     export PLUGIN_BUILDFILE="build.xml"
 fi
 
-echo "$SSH_KEY" > /root/.ssh/id_rsa && chmod 600 /root/.ssh/id_rsa
-
+mkdir /root/.ssh
+echo "$SSH_KEY" > /root/.ssh/id_rsa
+chmod 600 /root/.ssh/id_rsa
 echo "PROCESSING USING BUILDFILE $PLUGIN_BUILDFILE"
-
-echo "Execute phing now."
-exec phing -f $PLUGIN_BUILDFILE -Dsettings.tag=$DRONE_TAG
+phing -f $PLUGIN_BUILDFILE -Dsettings.tag=$DRONE_TAG
