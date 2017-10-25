2029693dbd275ec1f7b9638bd582df3937487104
Erik Seifert <erik.seifert@b-connect.de>
2017-10-25 09:20:34 +0200

    Add default build file

--- Dockerfile
+++ Dockerfile
@@ -1,3 +1,4 @@
 FROM bconnect/phing
 COPY runner.sh /runner.sh
+ADD build.xml /build.xml
 CMD ["/runner.sh"]
--- runner.sh
+++ runner.sh
@@ -1,12 +1,20 @@
 #!/bin/sh
 
 cd $DRONE_WORKSPACE
+
 if [ -z "$PLUGIN_BUILDFILE" ]; then
+    echo "No build file specified, try default build file.";
     export PLUGIN_BUILDFILE="build.xml"
+fi
+
+echo "Check for build file ${PLUGIN_BUILDFILE}"
+if [ ! -f "$PLUGIN_BUILDFILE" ]; then
+    echo "Using default build file provide by image."
+    export PLUGIN_BUILDFILE="/build.xml"
 fi
 
 mkdir /root/.ssh
 echo "$SSH_KEY" > /root/.ssh/id_rsa
 chmod 600 /root/.ssh/id_rsa
-echo "PROCESSING USING BUILDFILE $PLUGIN_BUILDFILE"
+echo "Start processing $PLUGIN_BUILDFILE ."
 phing -f $PLUGIN_BUILDFILE -Dsettings.tag=$DRONE_TAG
