9c57aba1643c37e9a778a1a54af0b1f381ff5aed
Erik Seifert <erik.seifert@b-connect.de>
2017-11-23 14:44:24 +0100

    Set correct project path

--- runner.sh
+++ runner.sh
@@ -1,14 +1,14 @@
 #!/bin/sh
 
-cd $DRONE_WORKSPACE
+cd $CI_PROJECT_DIR
 
-if [ -z "$PLUGIN_BUILDFILE" ]; then
+if [ -z "$PHING_BUILDFILE" ]; then
     echo "No build file specified, try default build file.";
     export PLUGIN_BUILDFILE="build.xml"
 fi
 
-echo "Check for build file ${PLUGIN_BUILDFILE}"
-if [ ! -f "$PLUGIN_BUILDFILE" ]; then
+echo "Check for build file ${PHING_BUILDFILE}"
+if [ ! -f "$PHING_BUILDFILE" ]; then
     echo "Using default build file provide by image."
     export PLUGIN_BUILDFILE="/build.xml"
 fi
@@ -18,5 +18,5 @@ cat /etc/ssh/ssh_config
 mkdir /root/.ssh
 echo "$SSH_KEY" > /root/.ssh/id_rsa
 chmod 600 /root/.ssh/id_rsa
-echo "Start processing $PLUGIN_BUILDFILE ."
-phing -f $PLUGIN_BUILDFILE -Dsettings.tag=$DRONE_TAG
+echo "Start processing $PHING_BUILDFILE ."
+phing -f $PHING_BUILDFILE -Dsettings.tag=$CI_COMMIT_TAG
