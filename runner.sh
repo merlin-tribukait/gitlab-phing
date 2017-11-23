9a7ffbc28d12bdb7a6920ae737444cae06e8674f
Erik Seifert <erik.seifert@b-connect.de>
2017-11-23 15:02:23 +0100

    Add correct build file

--- runner.sh
+++ runner.sh
@@ -2,21 +2,21 @@
 
 cd $CI_PROJECT_DIR
 
-if [ -z "$PHING_BUILDFILE" ]; then
+if [ -z "$PLUGIN_BUILDFILE" ]; then
     echo "No build file specified, try default build file.";
     export PLUGIN_BUILDFILE="build.xml"
 fi
 
 echo "Check for build file ${PHING_BUILDFILE}"
-if [ ! -f "$PHING_BUILDFILE" ]; then
+if [ ! -f "$PLUGIN_BUILDFILE" ]; then
     echo "Using default build file provide by image."
     export PLUGIN_BUILDFILE="/build.xml"
 fi
 
 
 mkdir /root/.ssh
-echo "SSH Key: $SSH_KEY" > /root/.ssh/id_rsa
-cat /root/.ssh/id_rsa
+echo "$SSH_KEY" > /root/.ssh/id_rsa
+
 chmod 600 /root/.ssh/id_rsa
-echo "Start processing $PHING_BUILDFILE for ${CI_COMMIT_TAG} ."
-phing -f $PHING_BUILDFILE -Dsettings.tag=$CI_COMMIT_TAG
+echo "Start processing $PHING_BUILDFILE ."
+phing -f $PLUGIN_BUILDFILE -Dsettings.tag=$CI_COMMIT_TAG
