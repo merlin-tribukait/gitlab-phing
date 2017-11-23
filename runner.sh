a4096e552ff60a64bc6c9ce4f3c8e3d1f6f251ad
Erik Seifert <erik.seifert@b-connect.de>
2017-11-23 15:06:35 +0100

    Update

--- runner.sh
+++ runner.sh
@@ -2,15 +2,15 @@
 
 cd $CI_PROJECT_DIR
 
-if [ -z "$PLUGIN_BUILDFILE" ]; then
+if [ -z "$PHING_BUILDFILE" ]; then
     echo "No build file specified, try default build file.";
-    export PLUGIN_BUILDFILE="build.xml"
+    export PHING_BUILDFILE="build.xml"
 fi
 
 echo "Check for build file ${PHING_BUILDFILE}"
-if [ ! -f "$PLUGIN_BUILDFILE" ]; then
+if [ ! -f "$PHING_BUILDFILE" ]; then
     echo "Using default build file provide by image."
-    export PLUGIN_BUILDFILE="/build.xml"
+    export PHING_BUILDFILE="/build.xml"
 fi
 
 
@@ -19,4 +19,4 @@ echo "$SSH_KEY" > /root/.ssh/id_rsa
 
 chmod 600 /root/.ssh/id_rsa
 echo "Start processing $PHING_BUILDFILE ."
-phing -f $PLUGIN_BUILDFILE -Dsettings.tag=$CI_COMMIT_TAG
+phing -f $PHING_BUILDFILE -Dsettings.tag=$CI_COMMIT_TAG
